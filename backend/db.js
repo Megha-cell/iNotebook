78f0e3c125b4200236332babc95436a756ce6532
Megha-cell <meghakamlekar123@gmail.com>
2025-01-05 22:23:00 +0530

    mongodb

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook"
+//const mongoURI = "mongodb://localhost:27017/inotebook"
+const mongoURI ="mongodb+srv://meghakamlekar123:XPBfMsrkOGhC5GJW@cluster0.v39vq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const connectToMongo = () => {
     mongoose.connect(mongoURI).then(() => {
         console.log("Connected to Mongo Successfully");
