ad6c34c455adafc8b88401c82b510da7268d635d
Megha-cell <meghakamlekar123@gmail.com>
2025-01-15 22:22:09 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ connectToMongo(); // Establish MongoDB connection
 const app = express();
 const port = process.env.PORT || 5000;
 //Available Routes
-app.use(cors())
+app.use(cors({ origin: 'https://i-notebook-one-delta.vercel.app' }))
 app.use(express.json())//t be able to use req.body
 
 
@@ -16,5 +16,5 @@ app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`iNotebook backend listening at http://localhost:${port}`);
+  console.log(`iNotebook backend listening at ${port}`);
 });
